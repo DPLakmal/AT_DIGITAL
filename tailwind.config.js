9a295a0fe5e969f33f05505133fb502ad1e1963c
Pubudu Lakmal <pubudu.lakmalc@gmail.com>
2024-09-04 19:17:56 +0530

    fix: style issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      extend: {
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'],
-        },
-        colors: {
-          primary_color: '#6B3CC9',
-          secondary_color: '#F28D35',
-          analogous_color_1: '#6A44F2',
-          analogous_color_2: '##1CBDDD',
-          triadic_color_1: '#52378C',
-        },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary_color: '#6B3CC9',
+        secondary_color: '#F28D35',
+        analogous_color_1: '#6A44F2',
+        analogous_color_2: '##1CBDDD',
+        triadic_color_1: '#52378C',
       },
     },
   },
